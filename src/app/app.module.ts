5fec9685b0dd1ba2655968eeb168d6971b7db2b2
17h51a05k5 <17h51a05k5@gmail.com>
2020-01-02 13:08:04 +0530

    second check in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    FeaturesComponent,
+    PricingComponent
   ],
   imports: [
     BrowserModule,
